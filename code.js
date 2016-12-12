16a00f485c057f0e6f4ae8c7333d9c46b475be57
Chung Oi Man <uhmon@Chungs-MacBook-Pro.local>
2016-12-12 14:32:41 +0800

    back to drawing board

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ var board = [null, null, null, null, null, null, null, null, null]
 
 var currentPlayer = 1
 
-function restart() {
+function restart () {
   for (i in board) {
     board[i] = null
   }
@@ -14,7 +14,7 @@ function playTurn (index) {
   if ((board[index] !== null) || (isGameOver() == true)) {
     return false
   }
-  board[index] = currentPlayer
+  board[index] = currentPlayer  // currentPlayer fills board[i] spot with its mark
   if (currentPlayer === 1) {
     currentPlayer = 2
   } else {
@@ -24,31 +24,42 @@ function playTurn (index) {
 }
 
 function whoWon () {
-
   for (var i = 0; i < 3; i++) {
-    if (board[i] === board[i+3] && board[i] === board[i+6] )
-    return board[i] //if column filled, win
-    if (board[i] === board[i+1] && board[i] == board[i+2] )
-    return board[i] //if row filled, win
-  } console.log(whoWhon([0, 3, 6]));
+    if ((board[i] === board[i + 3]) && (board[i] === board[i + 6])) {
+      return board[i] //if column filled, win
+    }
+  }
+  for (var i = 0; i <=6; i+=3 ) {
+  if ((board[i] === board[i + 1]) && (board[i] === board[i + 2])) {
+      return board[i] // if row filled, win
+    }
+  }
+  // console.log(whoWon([0, 3, 6]))
   if (board[0] === board[4] && board[0] === board[8] ||
-    board[2] === board[4] && board[2] === board[6])
-    return board[4]//check for the two diagonal conditions
-  if (isBoardFull())
+    board[2] === board[4] && board[2] === board[6]) {
+    return board[4] // check for the two diagonal conditions
+  }
+  if (isBoardFull()) {
     return 3
-}
+  }
+  return 0
+  }
+
 
 function isBoardFull () {
-  if (board.indexOf[null] === -1) {
-    return true
-  } else {
-    return false
+  for (var i in board) {
+    if (board[i] === null) {
+      return false
+    }
   }
+  return true
 }
 
-  function isGameOver () {
+function isGameOver () {
     if (isBoardFull() || whoWon() === 0) {
       return true
     }
     return false
   }
+board = [null,1,null,null,1,null,null,1,null]
+console.log(whoWon())
